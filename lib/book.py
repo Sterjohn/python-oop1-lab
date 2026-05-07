de981c8abcbacd581185bfa5884c81817d3beda4
Sterling Johnson <sterlingjohnson93@gmail.com>
2026-05-07 14:52:26 -0400

    tests passed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python3
+# Book class representing a book in the bookstore
 
 class Book:
-    pass
-    
-        
+
+    def __init__(self, title, page_count):
+        # Set the title and page_count when a Book is created
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        # Return the page count value
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        # Only allow integers for page_count
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        # Print a message when the user turns a page
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python3
+# Coffee class representing a coffee item in the bookstore
 
 class Coffee:
-    pass
+
+    def __init__(self, size, price):
+        # Set the size and price when a Coffee is created
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        # Return the size value
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        # Only allow Small, Medium, or Large for size
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    def tip(self):
+        # Print a thank you message and increase the price by 1
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
